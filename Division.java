57c03eadd3667362791b62cf86cad48f2fe76f36
anjaliram7 <69080715+anjaliram7@users.noreply.github.com>
2022-02-12 16:06:53 +0530

    Add files via upload

---
+++ Division.java
@@ -0,0 +1,13 @@
+public class Division
+{
+	public static void main(String[] args) {
+	    int num1, num2, quotient, remainder;
+	    num1 = 10;
+	    num2 =2;
+	    quotient= num1 / num2;
+	    remainder= num1 % num2;
+	    System.out.println("Quotient is " + quotient);
+	    System.out.println("Remainder is " + remainder);
+
+	}
+}
